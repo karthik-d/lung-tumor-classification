b6182095a059f347a37efc873dfbe97aca1a5576
Karthik-D <karthikdesingu2000@gmail.com>
2021-06-02 01:56:49 +0530

    Extnded to three models

--- UNet/merge_features.py
+++ UNet/merge_features.py
@@ -3,8 +3,8 @@ import pandas as pd
 model_names = [
 	'densenet',
 	'resnet',
-	#'inception'
-][:2]
+	'inception'
+]
 
 file_suffix = [
 	'_img_features_{model}.csv'.format(model=model_name)
@@ -16,14 +16,23 @@ merged = pd.read_csv(file_prefix + file_suffix[0])
 for count, suffix in enumerate(file_suffix[1:]):
 	filename = file_prefix + suffix
 	print(filename)
-	break
 	merged = merged.merge(
 		pd.read_csv(filename), 
 		on='Unnamed: 0', 
 		validate='one_to_one',
 		suffixes=('', '_{cnt}'.format(cnt=(count+1)))
 		)
-merged.to_csv('train.csv')
+# Add class labels
+merged = merged.merge(
+	pd.read_csv('labels.csv').loc[
+		:, ['image_name', 'target']
+	],
+	left_on='Unnamed: 0',
+	right_on='image_name',
+	validate='one_to_one'
+)
+merged.drop(columns=['Unnamed: 0'])
+merged.to_csv('train.csv', index=False)
 
 """
 merged = pd.read_csv('test1.csv')
@@ -34,10 +43,20 @@ for count, filename in enumerate(['test1.csv', 'test2.csv'][1:]):
 		validate='one_to_one',
 		suffixes=('', '_{cnt}'.format(cnt=(count+1)))
 		)
-
+# Add class labels
+merged = merged.merge(
+	pd.read_csv('test_labels.csv').loc[
+		:, ['image_name', 'target']
+	],
+	left_on='Unnamed: 0',
+	right_on='image_name',
+	validate='one_to_one'
+)
+merged.drop(columns=['Unnamed: 0'])
 merged.to_csv('test3.csv', index=False)
 print(merged)
 """
+
 
 	
 
